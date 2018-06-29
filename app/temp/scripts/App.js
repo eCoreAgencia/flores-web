4d187a30d2c2772b2aae4ed9ab3c15a558ebc7f6
Nicola <nicregattieri@gmail.com>
2018-06-29 19:11:25 -0300

    updated key

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -86,9 +86,9 @@ var _ColorHeader = __webpack_require__(4);
 
 var _ColorHeader2 = _interopRequireDefault(_ColorHeader);
 
-var _SlickCarousel = __webpack_require__(5);
+var _SlickCarouselousel = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./modules/SlickCarouselousel\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 
-var _SlickCarousel2 = _interopRequireDefault(_SlickCarousel);
+var _SlickCarouselousel2 = _interopRequireDefault(_SlickCarouselousel);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -96,7 +96,7 @@ var mobileMenu = new _MobileMenu2.default();
 var modal = new _Modal2.default();
 var generalCorrections = new _GeneralCorrections2.default();
 var colorHeader = new _ColorHeader2.default();
-var slickCarousel = new _SlickCarousel2.default();
+var slickCarousel = new _SlickCarouselousel2.default();
 
 /***/ }),
 /* 1 */
@@ -310,52 +310,6 @@ exports.default = ColorHeader;
 // this.primaryNavigation.mouseover(this.addColors.bind(this));
 // this.headerContainer.mouseout(this.removeColors.bind(this));
 // this.primaryNavigation.mouseout(this.removeColors.bind(this));
-
-/***/ }),
-/* 5 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var SlickCarousel = function () {
-    function SlickCarousel() {
-        _classCallCheck(this, SlickCarousel);
-
-        this.carousel = $('.slide');
-        startSlick();
-    }
-
-    _createClass(SlickCarousel, [{
-        key: 'startSlick',
-        value: function startSlick() {
-            this.carousel.slick({
-                adaptiveHeight: true,
-                autoplay: true,
-                autoplaySpeed: 5000,
-                pauseOnHover: false,
-                arrows: true,
-                dots: false,
-                draggable: true,
-                touchMove: true,
-                slidesToShow: 1,
-                slidesToScroll: 1
-            });
-        }
-    }]);
-
-    return SlickCarousel;
-}();
-
-exports.default = SlickCarousel;
 
 /***/ })
 /******/ ]);
