b0e436c2256c516493df79a59ab0ec29a9fad1f0
Nicola <nicregattieri@gmail.com>
2018-07-17 09:22:02 -0300

    go office

--- app/assets/scripts/modules/SlickCarousel.js
+++ app/assets/scripts/modules/SlickCarousel.js
@@ -3,7 +3,7 @@ class SlickCarousel {
         this.carousel = $('.slide');
         this.shelf = $('.shelf__slider .prateleira ul');
         this.startSlick();
-        this.startSlickShelf();
+        // this.startSlickShelf();
     }
     startSlick() {
         this.carousel.slick({
@@ -19,38 +19,38 @@ class SlickCarousel {
             slidesToScroll: 1
         });
     }
-    startSlickShelf(){
-         this.shelf.slick({
-            adaptiveHeight: true,
-            autoplay: false,
-            arrows: true,
-            dots: false,
-            mobileFirst: true,
-            draggable: true,
-            touchMove: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            responsive: [{
-                    breakpoint: 991,
-                    settings: {
-                        slidesToShow: 4
-                    }
-                },
-                {
-                    breakpoint: 767,
-                    settings: {
-                        slidesToShow: 3
-                    }
-                },
-                {
-                    breakpoint: 479,
-                    settings: {
-                        slidesToShow: 2
-                    }
-                }
-            ]
-        });
+    // startSlickShelf(){
+    //      this.shelf.slick({
+    //         adaptiveHeight: true,
+    //         autoplay: false,
+    //         arrows: true,
+    //         dots: false,
+    //         mobileFirst: true,
+    //         draggable: true,
+    //         touchMove: true,
+    //         slidesToShow: 1,
+    //         slidesToScroll: 1,
+    //         responsive: [{
+    //                 breakpoint: 991,
+    //                 settings: {
+    //                     slidesToShow: 4
+    //                 }
+    //             },
+    //             {
+    //                 breakpoint: 767,
+    //                 settings: {
+    //                     slidesToShow: 3
+    //                 }
+    //             },
+    //             {
+    //                 breakpoint: 479,
+    //                 settings: {
+    //                     slidesToShow: 2
+    //                 }
+    //             }
+    //         ]
+    //     });
 
-    }
+    // }
 }
 export default SlickCarousel;
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -330,7 +330,7 @@ var SlickCarousel = function () {
         this.carousel = $('.slide');
         this.shelf = $('.shelf__slider .prateleira ul');
         this.startSlick();
-        this.startSlickShelf();
+        // this.startSlickShelf();
     }
 
     _createClass(SlickCarousel, [{
@@ -349,37 +349,40 @@ var SlickCarousel = function () {
                 slidesToScroll: 1
             });
         }
-    }, {
-        key: 'startSlickShelf',
-        value: function startSlickShelf() {
-            this.shelf.slick({
-                adaptiveHeight: true,
-                autoplay: false,
-                arrows: true,
-                dots: false,
-                mobileFirst: true,
-                draggable: true,
-                touchMove: true,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                responsive: [{
-                    breakpoint: 991,
-                    settings: {
-                        slidesToShow: 4
-                    }
-                }, {
-                    breakpoint: 767,
-                    settings: {
-                        slidesToShow: 3
-                    }
-                }, {
-                    breakpoint: 479,
-                    settings: {
-                        slidesToShow: 2
-                    }
-                }]
-            });
-        }
+        // startSlickShelf(){
+        //      this.shelf.slick({
+        //         adaptiveHeight: true,
+        //         autoplay: false,
+        //         arrows: true,
+        //         dots: false,
+        //         mobileFirst: true,
+        //         draggable: true,
+        //         touchMove: true,
+        //         slidesToShow: 1,
+        //         slidesToScroll: 1,
+        //         responsive: [{
+        //                 breakpoint: 991,
+        //                 settings: {
+        //                     slidesToShow: 4
+        //                 }
+        //             },
+        //             {
+        //                 breakpoint: 767,
+        //                 settings: {
+        //                     slidesToShow: 3
+        //                 }
+        //             },
+        //             {
+        //                 breakpoint: 479,
+        //                 settings: {
+        //                     slidesToShow: 2
+        //                 }
+        //             }
+        //         ]
+        //     });
+
+        // }
+
     }]);
 
     return SlickCarousel;
